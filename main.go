bb349f9e5c2a5442e094b6e63e3a021265a3985e
DHAIRYADHOLU <dhairyadholu@gmail.com>
2023-09-22 00:12:41 -0400

    Function by Dhruvi Shah

--- main.go
+++ main.go
@@ -21,7 +21,12 @@ func multiplyAndDivide(a, b int) (int, float64) {
     return product, quotient
 }
 
+func IsDivisibleBy7(num int) bool {
+	return num%7 == 0
+}
+
 func main() {
+	
 	num1 := 10
 	num2 := 5
 
@@ -29,14 +34,25 @@ func main() {
 
 	fmt.Printf("Minimum: %d\n", min)
 	fmt.Printf("Maximum: %d\n", max)
-
+//////////////////////////////////////////////////////////////////
 	v1, v2 := swap("kapoor", "vinay")
 	fmt.Println(v1, v2)
-
+//////////////////////////////////////////////////////////////////
 	x := 10
     y := 3
     result1, result2 := multiplyAndDivide(x, y)
     fmt.Printf("Product: %d, Quotient: %.2f\n", result1, result2)
-
+///////////////////////////////////////////////////////////////////
+	var number int
+	fmt.Print("Enter a number: ")
+	fmt.Scan(&number)
+		if IsDivisibleBy7(number) 
+		{
+			fmt.Printf("%d is divisible by 7\n", number)
+		} 
+		else 
+		{
+			fmt.Printf("%d is not divisible by 7\n", number)
+		}
 
 }
